66d9abf05877dcfb10f1aaa4acb103fbd17cd9ca
pyadminru <zh.vitaliy92@yandex.ru>
2026-02-24 16:05:40 +0300

    Рефакторинг кода

--- account_manager/parser.py
+++ account_manager/parser.py
@@ -315,8 +315,8 @@ async def join_required_channels(client, user_id, message):
 
     for channel in list(db_channels - already_subscribed)[:500]:  # Ограничиваем до 500 записей
         random_delay = random.choice(
-            [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
-             38, 39, 40]
+            [15, 16, 17, 18, 19, 20, 21, 22, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
+             38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
         )
         try:
             logger.warning(f"🔗 Подписка на {channel}")
