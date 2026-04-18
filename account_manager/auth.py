5e86dd097d65d50b204eff1181f77e2129382c3f
PyRusAdmin <zh.vitaliy92@yandex.ru>
2026-04-19 00:12:36 +0300

    Рефакторинг кода

--- account_manager/auth.py
+++ account_manager/auth.py
@@ -5,7 +5,6 @@ import os
 import random
 from pathlib import Path
 
-import python_socks.async_.asyncio
 from aiogram.types import Message
 from loguru import logger
 from telethon import TelegramClient
@@ -68,11 +67,6 @@ class CheckingAccountsValidity:
         :param session_name: Имя аккаунта для подключения (файл .session)
         :return: Клиент Telegram или None, если подключение не удалось
         """
-        setup_proxy()
-        # Проверяем, есть ли MTProxy конфиг
-        # use_mtproxy = bool(MT_PROXY_IP and MT_PROXY_SECRET)
-
-        # if use_mtproxy:
         logger.info(f"🔗 Подключение через MTProxy: {MT_PROXY_IP}:{443}")
         client = TelegramClient(
             StringSession(session_name),
@@ -86,18 +80,6 @@ class CheckingAccountsValidity:
             # connection=connection.ConnectionTcpMTProxyAbridged,
             proxy=('socks5', "87.239.250.238", 39301, True, "C8MRdMvXh8", "FmUmOnx2JW")
         )
-        # else: 103.59.42.72:443:7867291:7mTUvwFmRDbG3BcjRHXtyNB0YXhpLnlhbmRleC5ydQ
-        # logger.info("📡 Подключение без прокси")
-        # client = TelegramClient(
-        #     StringSession(session_name),
-        #     api_id=API_ID,
-        #     api_hash=API_HASH,
-        #     device_model=mobile_device["device_model"],
-        #     system_version=mobile_device["system_version"],
-        #     app_version=mobile_device["app_version"],
-        #     lang_code=mobile_device["lang_code"],
-        #     system_lang_code=mobile_device["system_lang_code"]
-        # )
 
         try:
             await client.connect()
@@ -152,7 +134,6 @@ class CheckingAccountsValidity:
         :param session_name: Имя аккаунта для проверки
         """
         try:
-            setup_proxy()
             logger.info(f"Проверка аккаунта {session_name}")
             client: TelegramClient = await self.client_connect_string_session(session_name=session_name)
 
@@ -194,12 +175,6 @@ class CheckingAccountsValidity:
         Подключение клиента Telethon и проверка сессии.
         :return: client или None, если сессия невалидна
         """
-        setup_proxy()
-        # if not self.path:
-        #     logger.error("❌ self.path не установлен для connect_client()")
-        #     return None
-        # use_mtproxy = bool(MT_PROXY_IP and MT_PROXY_SECRET)
-        # if use_mtproxy:
         client = TelegramClient(
             self.path,
             api_id=API_ID,
@@ -212,17 +187,6 @@ class CheckingAccountsValidity:
             # connection=connection.ConnectionTcpMTProxyAbridged,
             proxy=('socks5', "87.239.250.238", 39301, True, "C8MRdMvXh8", "FmUmOnx2JW")
         )
-        # else:
-        #     client = TelegramClient(
-        #         self.path,
-        #         api_id=API_ID,
-        #         api_hash=API_HASH,
-        #         device_model=mobile_device["device_model"],
-        #         system_version=mobile_device["system_version"],
-        #         app_version=mobile_device["app_version"],
-        #         lang_code=mobile_device["lang_code"],
-        #         system_lang_code=mobile_device["system_lang_code"]
-        #     )
         try:
             await client.connect()
 
@@ -251,15 +215,6 @@ class CheckingAccountsValidity:
 
             logger.info(f"Используется сессия: {chosen_session_name[:30]}...")
 
-            proxy = {
-                'proxy_type': python_socks.ProxyType.SOCKS5,
-                'addr': "87.239.250.238",
-                'port': 39301,
-                'username': 'C8MRdMvXh8',
-                'password': 'FmUmOnx2JW',
-                'rdns': True
-            }
-
             client = TelegramClient(
                 StringSession(chosen_session_name),
                 api_id=API_ID,
@@ -270,7 +225,6 @@ class CheckingAccountsValidity:
                 lang_code=mobile_device["lang_code"],
                 system_lang_code=mobile_device["system_lang_code"],
                 timeout=30,
-                # proxy=proxy
             )
             await client.connect()
 
